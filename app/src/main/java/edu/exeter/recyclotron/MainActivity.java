1306511fe6adb6287a623f080c70e179a314386d
RayaBurong25-1 <arun92166@gmail.com>
2019-02-05 22:35:10 -0500

    Add tabs (needs debugging)

--- app/src/main/java/edu/exeter/recyclotron/MainActivity.java
+++ app/src/main/java/edu/exeter/recyclotron/MainActivity.java
@@ -3,20 +3,34 @@ package edu.exeter.recyclotron;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TabAdapter adapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        adapter = new TabAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Tab1Fragment(), "Tab 1");
+        adapter.addFragment(new Tab2Fragment(), "Tab 2");
+        adapter.addFragment(new Tab3Fragment(), "Tab 3");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
         //TODO: left side = scan QR code
         //TODO: center side = dashboard (username, points, social media buttons)
         //TODO: right side = partnership market
     }
-
 }
